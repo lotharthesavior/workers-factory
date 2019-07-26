30d7ce615c71f6d74079eb84be70265bb640a4b8
Savio Resende <savio@savioresende.com.br>
2019-07-26 08:09:04 -0400

    Added build

---
+++ build/precache-manifest.98e289a540805572b9b823b4a07fff7c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7399036041592d09414d1ebf441d2dc7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "61a80b8716ff498365f0",
+    "url": "/static/css/main.4e970852.chunk.css"
+  },
+  {
+    "revision": "7b1d08845570d89aabac",
+    "url": "/static/js/2.d432ccb2.chunk.js"
+  },
+  {
+    "revision": "61a80b8716ff498365f0",
+    "url": "/static/js/main.da3cfd71.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
